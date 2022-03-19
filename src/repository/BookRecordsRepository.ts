0beb4752c100432101496a1c9e79891f53713ff2
okaryo <okaryotr@gmail.com>
2022-03-19 23:17:14 +0900

    [add] repository

---
+++ src/repository/BookRecordsRepository.ts
@@ -0,0 +1,24 @@
+import { BookRecords } from "../model/BookRecords";
+import BookRecordsJson from "../data/book-list.json"
+import { BookRecord } from "../model/BookRecord";
+import { Book } from "../model/Book";
+import { Author } from "../model/Author";
+
+export class BookRecordsRepository {
+  static getBookRecords = (): BookRecords => {
+    const json = BookRecordsJson
+    const records = json.records.map(record => {
+      return new BookRecord(
+        new Date(record.date),
+        record.review,
+        new Book(
+          record.book.page,
+          record.book.title,
+          record.book.url,
+          new Author(record.book.author.name)
+        )
+      )
+    })
+    return new BookRecords(records)
+  }
+}
