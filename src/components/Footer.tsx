713f0dfd8d4843ffe7d04dcafe573d5523c60e7a
okaryo <okaryotr@gmail.com>
2022-03-19 23:16:35 +0900

    [fix] footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <Typography variant="caption" component="p">
           Copyright ©{new Date().getFullYear()}{' '}
           <Link
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://okaryo.io"
             target="_blank"
             rel="noopener noreferrer"
             underline="none"
