fd5c7358fff2feb57c60bb3d0b2653f098426cac
okaryo <okaryotr@gmail.com>
2022-03-26 21:21:34 +0900

    [modify] site meta data

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -3,14 +3,14 @@ import Head from 'next/head'
 const Meta = () => {
   return (
     <Head>
-      <title>OKARYO BOOK</title>
+      <title>OKARYO BOOKSHELF</title>
       <meta name="theme-color" content="#000000" />
-      <meta name="description" content="okaryo's bookshelf" />
+      <meta name="description" content="okaryoの本棚" />
       <meta property="og:url" content="https://book.okaryo.io" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="OKARYO BOOK" />
-      <meta property="og:description" content="okaryo's bookshelf" />
-      <meta property="og:site_name" content="OKARYO BOOK" />
+      <meta property="og:title" content="OKARYO BOOKSHELF" />
+      <meta property="og:description" content="okaryoの本棚" />
+      <meta property="og:site_name" content="OKARYO BOOKSHELF" />
       <meta property="og:image" content="https://book.okaryo.io/ogp.png" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
