e38448c65c6d040666d19999f0c8307f59a85c53
okaryo <okaryotr@gmail.com>
2022-03-27 15:18:35 +0900

    [modify] BookList component for state

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,38 +1,38 @@
-import { Container, Link, List, ListItem, ListItemButton, ListItemText } from '@mui/material'
-import { BookRecordsRepository } from '../repository/BookRecordsRepository'
+import { Link, List, ListItem, ListItemButton, ListItemText } from '@mui/material'
+import { useSelector } from 'react-redux'
+import { RootState } from '../store'
 
 const BookList = () => {
-  return (
-    <Container sx={{p: 2}}>
-      <List>
-        {
-          BookRecordsRepository.getBookRecords().values.map((record, index) => {
-            const dateText = record.formatDate
-            return (
-              <ListItem key={index} style={{borderBottom: '1px solid #D2D2D2'}} disablePadding >
-                <ListItemButton style={{ width: '100%' }} sx={{pr: 0, pl: 0}}>
-                  <ListItemText
-                    style={{ overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
-                    primary={
-                      <Link
-                        href={record.book.url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        underline="none"
-                      >
-                        {record.book.title}
-                      </Link>
-                    }
-                    secondary={dateText}
-                  />
-                </ListItemButton>
-              </ListItem>
-            )
-          })
-        }
-      </List>
+  const bookRecords = useSelector((state: RootState) => state.domain.bookRecords)
 
-    </Container>
+  return (
+    <List>
+      {
+        bookRecords.values.map((record, index) => {
+          const dateText = record.formatDate
+          return (
+            <ListItem key={index} style={{borderBottom: '1px solid #D2D2D2'}} disablePadding >
+              <ListItemButton sx={{pr: 0, pl: 0}}>
+                <ListItemText
+                  style={{ overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}
+                  primary={
+                    <Link
+                      href={record.book.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      underline="none"
+                    >
+                      {record.book.title}
+                    </Link>
+                  }
+                  secondary={dateText}
+                />
+              </ListItemButton>
+            </ListItem>
+          )
+        })
+      }
+    </List>
   )
 }
 
