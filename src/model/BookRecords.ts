5338011905fdf6c8cdb19410d52430c677af193a
okaryo <okaryotr@gmail.com>
2022-03-27 15:16:27 +0900

    [modify] BookRecords to add some methods

--- src/model/BookRecords.ts
+++ src/model/BookRecords.ts
@@ -5,6 +5,10 @@ export class BookRecords {
     readonly values: BookRecord[]
   ) {}
 
+  static init(): BookRecords {
+    return new BookRecords([])
+  }
+
   get totalCount(): number {
     return this.values.length
   }
@@ -12,6 +16,42 @@ export class BookRecords {
   get totalPage(): number {
     return this.values.reduce((sum, record) => {
       return sum + record.page
+    }, 0)
+  }
+
+  totalCountByYear(year: number): number {
+    return this.values.reduce((sum, record) => {
+      if (record.date !== null && record.date.getFullYear() === year) {
+        return sum + 1
+      }
+      return sum
+    }, 0)
+  }
+
+  totalPageByYear(year: number): number {
+    return this.values.reduce((sum, record) => {
+      if (record.date !== null && record.date.getFullYear() === year) {
+        return sum + record.page
+      }
+      return sum
+    }, 0)
+  }
+
+  totalCountByMonth(year: number, month: number): number {
+    return this.values.reduce((sum, record) => {
+      if (record.date !== null && record.date.getFullYear() === year && record.date.getMonth() + 1 === month) {
+        return sum + 1
+      }
+      return sum
+    }, 0)
+  }
+
+  totalPageByMonth(year: number, month: number): number {
+    return this.values.reduce((sum, record) => {
+      if (record.date !== null && record.date.getFullYear() === year && record.date.getMonth() + 1 === month) {
+        return sum + record.page
+      }
+      return sum
     }, 0)
   }
 }
--- test/model/BookRecords.test.ts
+++ test/model/BookRecords.test.ts
@@ -3,6 +3,15 @@ import { Book } from "../../src/model/Book"
 import { BookRecord } from "../../src/model/BookRecord"
 import { BookRecords } from "../../src/model/BookRecords"
 
+describe('#init', () => {
+  test('should return empty book records', () => {
+    const actual = BookRecords.init()
+    const expected = new BookRecords([])
+
+    expect(actual).toStrictEqual(expected)
+  })
+})
+
 describe('#totalCount', () => {
   test('should return total count of records', () => {
     const actual = new BookRecords([
@@ -38,6 +47,106 @@ describe('#totalPage', () => {
       ),
     ]).totalPage
     const expected = 20
+
+    expect(actual).toBe(expected)
+  })
+})
+
+describe('#totalCountByYear', () => {
+  test('should return total count of records by year', () => {
+    const actual = new BookRecords([
+      new BookRecord(
+        new Date(2020, 3, 20),
+        'review',
+        new Book(10, 'title', 'url', new Author('name'))
+      ),
+      new BookRecord(
+        new Date(2021, 3, 20),
+        'review',
+        new Book(10, 'title', 'url', new Author('name'))
+      ),
+      new BookRecord(
+        new Date(2022, 3, 20),
+        'review',
+        new Book(10, 'title', 'url', new Author('name'))
+      ),
+    ]).totalCountByYear(2022)
+    const expected = 1
+
+    expect(actual).toBe(expected)
+  })
+})
+
+describe('#totalPageByYear', () => {
+  test('should return total pages of records by year', () => {
+    const actual = new BookRecords([
+      new BookRecord(
+        new Date(2020, 3, 20),
+        'review',
+        new Book(13, 'title', 'url', new Author('name'))
+      ),
+      new BookRecord(
+        new Date(2021, 3, 20),
+        'review',
+        new Book(6, 'title', 'url', new Author('name'))
+      ),
+      new BookRecord(
+        new Date(2022, 3, 20),
+        'review',
+        new Book(30, 'title', 'url', new Author('name'))
+      ),
+    ]).totalPageByYear(2022)
+    const expected = 30
+
+    expect(actual).toBe(expected)
+  })
+})
+
+describe('#totalCountByMonth', () => {
+  test('should return total count of records by month', () => {
+    const actual = new BookRecords([
+      new BookRecord(
+        new Date(2022, 1, 20),
+        'review',
+        new Book(13, 'title', 'url', new Author('name'))
+      ),
+      new BookRecord(
+        new Date(2022, 2, 20),
+        'review',
+        new Book(6, 'title', 'url', new Author('name'))
+      ),
+      new BookRecord(
+        new Date(2022, 3, 20),
+        'review',
+        new Book(30, 'title', 'url', new Author('name'))
+      ),
+    ]).totalCountByMonth(2022, 3)
+    const expected = 1
+
+    expect(actual).toBe(expected)
+  })
+})
+
+describe('#totalPageByMonth', () => {
+  test('should return total pages of records by month', () => {
+    const actual = new BookRecords([
+      new BookRecord(
+        new Date('2022-01-20'),
+        'review',
+        new Book(13, 'title', 'url', new Author('name'))
+      ),
+      new BookRecord(
+        new Date('2022-02-20'),
+        'review',
+        new Book(6, 'title', 'url', new Author('name'))
+      ),
+      new BookRecord(
+        new Date('2022-03-20'),
+        'review',
+        new Book(31, 'title', 'url', new Author('name'))
+      ),
+    ]).totalPageByMonth(2022, 3)
+    const expected = 31
 
     expect(actual).toBe(expected)
   })
