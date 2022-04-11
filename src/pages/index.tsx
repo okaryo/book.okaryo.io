5a1dd3bd5bf0b6ca26e58bbac0b73a09c514c561
okaryo <okaryotr@gmail.com>
2022-04-11 22:02:02 +0900

    [modify] add ga tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
+import Script from 'next/script'
 import Summary from '../components/Summary'
 
 const Home = () => {
-  return <Summary />
+  return (
+    <div>
+      <Summary />
+
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script
+        src='https://www.googletagmanager.com/gtag/js?id=G-CQJMP9EC41'
+        strategy='afterInteractive'
+      />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-CQJMP9EC41');
+        `}
+      </Script>
+    </div>
+  )
 }
 
 export default Home
