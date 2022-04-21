2a59ba6ac98372a89a5afd5df8dfa2471753a3a9
okaryo <okaryotr@gmail.com>
2022-04-22 00:41:39 +0900

    [add] Pagination

---
+++ src/model/Pagination.ts
@@ -0,0 +1,25 @@
+import { Iterable } from './Iterable'
+
+export class Pagination<T> {
+  constructor(
+    private values: Iterable<T>,
+    readonly perPage: number = 20,
+    readonly current: number = 1,
+    readonly prev: number = 0,
+    readonly next: number = 2,
+  ) {}
+
+  get currentPageValues(): T[] {
+    const start = (this.current - 1) * this.perPage
+    const end = start + this.perPage
+    return this.values.slice(start, end).values
+  }
+
+  get length(): number {
+    return Math.ceil(this.values.length / this.perPage)
+  }
+
+  toPage(page: number): Pagination<T> {
+    return new Pagination(this.values, this.perPage, page, page - 1, page + 1)
+  }
+}
---
+++ test/model/Pagination.test.ts
@@ -0,0 +1,117 @@
+import { Author } from "../../src/model/Author"
+import { Book } from "../../src/model/Book"
+import { BookRecord } from "../../src/model/BookRecord"
+import { BookRecords } from "../../src/model/BookRecords"
+import { Pagination } from "../../src/model/Pagination"
+
+describe('#currentPageValues', () => {
+  test('should return current values', () => {
+    const actual = new Pagination(
+      new BookRecords([
+        new BookRecord(
+          new Date(2022, 3, 20),
+          'review',
+          new Book(10, 'title', 'url', new Author('name')),
+          'Paper',
+          false
+        ),
+        new BookRecord(
+          new Date(2022, 3, 20),
+          'review',
+          new Book(10, 'title', 'url', new Author('name')),
+          'Paper',
+          false
+        ),
+      ]),
+      1,
+      2,
+    ).currentPageValues
+    const expected = [
+      new BookRecord(
+        new Date(2022, 3, 20),
+        'review',
+        new Book(10, 'title', 'url', new Author('name')),
+        'Paper',
+        false
+      ),
+    ]
+
+    expect(actual).toEqual(expected)
+  })
+})
+
+describe('#length', () => {
+  test('should return 2 when perPage is 1, and values length is 2', () => {
+    const actual = new Pagination(
+      new BookRecords([
+        new BookRecord(
+          new Date(2022, 3, 20),
+          'review',
+          new Book(10, 'title', 'url', new Author('name')),
+          'Paper',
+          false
+        ),
+        new BookRecord(
+          new Date(2022, 3, 20),
+          'review',
+          new Book(10, 'title', 'url', new Author('name')),
+          'Paper',
+          false
+        ),
+      ]),
+      1,
+      2,
+    ).length
+    const expected = 2
+
+    expect(actual).toEqual(expected)
+  })
+})
+
+describe('#toPage', () => {
+  test('should return new pagination', () => {
+    const actual = new Pagination(
+      new BookRecords([
+        new BookRecord(
+          new Date(2022, 3, 20),
+          'review',
+          new Book(10, 'title', 'url', new Author('name')),
+          'Paper',
+          false
+        ),
+        new BookRecord(
+          new Date(2022, 3, 20),
+          'review',
+          new Book(10, 'title', 'url', new Author('name')),
+          'Paper',
+          false
+        ),
+      ]),
+      1,
+    ).toPage(2)
+    const expected = new Pagination(
+      new BookRecords([
+        new BookRecord(
+          new Date(2022, 3, 20),
+          'review',
+          new Book(10, 'title', 'url', new Author('name')),
+          'Paper',
+          false
+        ),
+        new BookRecord(
+          new Date(2022, 3, 20),
+          'review',
+          new Book(10, 'title', 'url', new Author('name')),
+          'Paper',
+          false
+        ),
+      ]),
+      1,
+      2,
+      1,
+      3,
+    )
+
+    expect(actual).toEqual(expected)
+  })
+})
