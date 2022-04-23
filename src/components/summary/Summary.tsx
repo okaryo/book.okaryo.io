ab34eec9152cf0cbfaf289d3ee2b33de2bb56e7a
okaryo <okaryotr@gmail.com>
2022-04-23 11:08:56 +0900

    [moidfy] SummaryPage to add last year record

--- src/components/summary/Summary.tsx
+++ src/components/summary/Summary.tsx
@@ -1,10 +1,28 @@
 import { Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import { store } from '../../store'
 
+type TableBodyRowProps = {
+  label: string,
+  count: number,
+  page: number
+}
+
 const Summary = () => {
   const bookRecords = store
   const currentDate = new Date()
+  const lastYearDate = new Date(currentDate.getFullYear() - 1, currentDate.getMonth(), currentDate.getDate())
   const lastMonthDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, currentDate.getDate())
+
+  const TableBodyRow = (props: TableBodyRowProps) => {
+    const { label, count, page } = props
+    return (
+      <TableRow sx={{'&:last-child td, &:last-child th': { border: 0 } }}>
+        <TableCell component='th' scope='row'>{label}</TableCell>
+        <TableCell align='center'>{count}冊</TableCell>
+        <TableCell align='center'>{page}ページ</TableCell>
+      </TableRow>
+    )
+  }
 
   return (
     <Stack>
@@ -13,35 +31,17 @@ const Summary = () => {
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell>サマリー</TableCell>
-                <TableCell align="center">冊数</TableCell>
-                <TableCell align="center">ページ</TableCell>
+                <TableCell sx={{fontWeight: 600}}>サマリー</TableCell>
+                <TableCell sx={{fontWeight: 600}} align='center'>冊数</TableCell>
+                <TableCell sx={{fontWeight: 600}} align='center'>ページ</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              <TableRow sx={{'&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell component="th" scope="row">累計記録</TableCell>
-                <TableCell align="center">{bookRecords.totalCount}冊</TableCell>
-                <TableCell align="center">{bookRecords.totalPage}ページ</TableCell>
-              </TableRow>
-
-              <TableRow sx={{'&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell component="th" scope="row">今年の記録</TableCell>
-                <TableCell align="center">{bookRecords.totalCountByYear(currentDate)}冊</TableCell>
-                <TableCell align="center">{bookRecords.totalPageByYear(currentDate)}ページ</TableCell>
-              </TableRow>
-
-              <TableRow sx={{'&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell component="th" scope="row">今月の記録</TableCell>
-                <TableCell align="center">{bookRecords.totalCountByMonth(currentDate)}冊</TableCell>
-                <TableCell align="center">{bookRecords.totalPageByMonth(currentDate)}ページ</TableCell>
-              </TableRow>
-
-              <TableRow sx={{'&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell component="th" scope="row">先月の記録</TableCell>
-                <TableCell align="center">{bookRecords.totalCountByMonth(lastMonthDate)}冊</TableCell>
-                <TableCell align="center">{bookRecords.totalPageByMonth(lastMonthDate)}ページ</TableCell>
-              </TableRow>
+              <TableBodyRow label={'累計記録'} count={bookRecords.totalCount} page={bookRecords.totalPage} />
+              <TableBodyRow label={'昨年の記録'} count={bookRecords.totalCountByYear(lastYearDate)} page={bookRecords.totalPageByYear(lastYearDate)} />
+              <TableBodyRow label={'今年の記録'} count={bookRecords.totalCountByYear(currentDate)} page={bookRecords.totalPageByYear(currentDate)} />
+              <TableBodyRow label={'先月の記録'} count={bookRecords.totalCountByMonth(lastMonthDate)} page={bookRecords.totalPageByMonth(lastMonthDate)} />
+              <TableBodyRow label={'今月の記録'} count={bookRecords.totalCountByMonth(currentDate)} page={bookRecords.totalPageByMonth(currentDate)} />
             </TableBody>
           </Table>
         </TableContainer>
